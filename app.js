478ca27c6775b2dc1d0d5a9ec62ceb2691d518a7
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-02-10 19:49:48 +0600

    Express-x

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import mongoose from "mongoose";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import hpp from 'hpp'
+import cors from 'cors'
+import {cookie} from "cookie-parser";
+
+const app=express();
+
+//Middleware
+app.use(cors())
+app.use(helmet())
+app.use(hpp())
+
+const  limiter=rateLimit({windowMs:15*60*100, max:3000})
+
+app.use(limiter);
+
+//Disable Response Cache
